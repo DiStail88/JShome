0dc67e840fd8f2637aacbebd40c64672150c6d51
DiStail88 <lebwebslav@gmail.com>
2025-03-14 19:18:22 +0300

    Ошибка при написании комента

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 import { renderComments } from "./modules/renderComments.js";
 import { replaceDangerSymbol } from "./modules/replaceSymbol.js";
-import { comments } from "./modules/comments.js";
+import { updateComments } from "./modules/comments.js";
 
-renderComments();
+fetch("https://wedev-api.sky.pro/api/v1/slava-leb/comments", {
+    method: "GET",
+})
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+        updateComments(data.comments);
+        renderComments();
+    });
 
 const buttonEl = document.getElementById("button");
 
 buttonEl.addEventListener("click", () => {
     const inputElName = document.getElementById("name");
     const inputElComment = document.getElementById("comment");
-
     const name = replaceDangerSymbol(inputElName.value);
     const comment = replaceDangerSymbol(inputElComment.value);
-
-    const currentDateTime = new Date();
-    const day = String(currentDateTime.getDate()).padStart(2, "0");
-    const month = String(currentDateTime.getMonth() + 1).padStart(2, "0");
-    const year = String(currentDateTime.getFullYear()).slice(-2);
-    const hours = String(currentDateTime.getHours()).padStart(2, "0");
-    const minutes = String(currentDateTime.getMinutes()).padStart(2, "0");
-
-    const formattedDateTime = `${day}.${month}.${year} ${hours}:${minutes}`;
 
     if (inputElName.value === "" && inputElComment.value === "") {
         inputElName.classList.add("error");
@@ -29,15 +29,33 @@ buttonEl.addEventListener("click", () => {
     }
 
     const NewComment = {
-        name: name,
-        dateTime: formattedDateTime,
-        commentText: comment,
-        likes: 0,
-        liked: false,
+        text: comment,
+        author: {
+            name: name,
+        },
     };
-    comments.push(NewComment);
 
-    renderComments();
+    fetch("https://wedev-api.sky.pro/api/v1/slava-leb/comments", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(NewComment),
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Ошибка сети: " + response.status);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log("Новый комментарий добавлен:", data);
+            updateComments(data.comments || data);
+            renderComments();
+        })
+        .catch((error) => {
+            console.error("Ошибка при добавлении комментария:", error);
+        });
 
     inputElName.value = "";
     inputElComment.value = "";
